28d97636990896568dca578dde3239beca8949fd
JesusManzano24 <9991404825j@gmail.com>
2024-03-05 21:29:37 -0600

    Segundo Commit Funciones Loggin

--- app/src/main/java/com/jesusmanzano/reproductor/MainActivity.java
+++ app/src/main/java/com/jesusmanzano/reproductor/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jesusmanzano.reproductor;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Inicio = findViewById(R.id.Inicio);
+        Inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity2.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
 }
