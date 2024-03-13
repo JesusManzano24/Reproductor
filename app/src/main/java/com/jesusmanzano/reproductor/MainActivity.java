39dba957221d1f550f73afcda12a94ac55e28d70
JesusManzano24 <9991404825j@gmail.com>
2024-03-12 18:15:26 -0600

    Septimo Commit Loggin Contraseña

--- app/src/main/java/com/jesusmanzano/reproductor/MainActivity.java
+++ app/src/main/java/com/jesusmanzano/reproductor/MainActivity.java
@@ -1,28 +1,42 @@
 package com.jesusmanzano.reproductor;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextUser;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTextUser = findViewById(R.id.user);
+        editTextPassword = findViewById(R.id.password);
+
         Button Inicio = findViewById(R.id.Inicio);
         Inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), MainActivity2.class);
-                startActivityForResult(intent, 0);
+                String user = editTextUser.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Verifica las credenciales estáticas (por ejemplo, usuario: "admin", contraseña: "1234")
+                if (user.equals("jesus") && password.equals("1234")) {
+                    Intent intent = new Intent(v.getContext(), MainActivity2.class);
+                    startActivity(intent);
+                } else {
+                    // Muestra un mensaje de error si las credenciales son incorrectas
+                    Toast.makeText(MainActivity.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
 }
